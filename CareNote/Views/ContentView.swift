d25972f854a920d09f370698543430d8a1cd962e
Christian Schinkel <christian.schinkel@me.com>
2022-09-12 17:30:15 +0200

    Second Working state of CareNote

---
+++ CareNote/Views/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  CareNote
+//
+//  Created by Christian Schinkel on 2022-07-10.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    var body: some View {
+        MainMenuTabView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
