8f627b8df4542a4421d0718f59dd58a41960d546
Christian Schinkel <christian.schinkel@me.com>
2022-09-29 23:50:55 +0200

    I added some enumerations in "CareData". I have not figured out how to implement enums in a collection to use them with a picker. //TO DO: - Friday the 30 september 2022 Make enums usable in a picker.

--- CareNote/DataModel/Data/CareData.swift
+++ CareNote/DataModel/Data/CareData.swift
@@ -16,6 +16,10 @@ struct Care {
         "LRV"
     ]
     
+    enum LawsEnumserDeklarerad: String {
+        case HSL, LPT, LVM, LVU, LRV
+    }
+    
     let howPatientComeIn = [
         "Police",
         "LOB",
@@ -26,6 +30,16 @@ struct Care {
         "Remiss from"
     ]
     
+    enum HowPatientComeInEnumserDeklarerad: String {
+        case police = "Police"
+        case lob = "LOB"
+        case lptParagraph47 = "LPT §47"
+        case ss = "SS"
+        case amb = "AMB"
+        case pam = "PAM"
+        case remissFrom = "Remiss from"
+    }
+    
     let suicidalRiskNiveau = [
         "low",
         "little",
@@ -33,11 +47,19 @@ struct Care {
         "high"
     ]
     
+    enum SuicidalRiskNiveauEnumserDeklarerad: String {
+        case low, little, medium, high
+    }
+    
     let violenceRiskNiveau = [
         "low",
         "medium",
         "high"
     ]
+    
+    enum ViolenceRiskNiveauEnumserDeklarerad: String {
+        case low, medium, high
+    }
     
     let typesOfObservation = [
         "normal",
@@ -45,6 +67,13 @@ struct Care {
         "X-VAK",
         "X-VAK/X-OBS"
     ]
+    
+    enum TypesOfObservationEnumserDeklarerad: String {
+        case normal = "normal"
+        case xobs = "X-OBS"
+        case xvak = "X-VAK"
+        case xvakXobs = "X-VAK/X-OBS"
+    }
     
     struct Medicine {
         let form = [
@@ -102,7 +131,40 @@ struct Care {
         let startDate = Date.now
     }
     
-    enum medicineForm {
-    case capsule
+    enum MedicineForm: String {
+        case capsule, tablet, liquid, topical, cream, device, drops, foam, gel, inhaler, injection, lotion, ointment, patch, powder, spray, suppository
     }
+    
+    enum MedicineUnit: String {
+        case milliGram = "mg"
+        case mikroGram = "µg"
+        case gram = "g"
+        case milliLiter = "ml"
+        case procent = "%"
+    }
+    
+    enum MedicineFrequency: String {
+        case atRegularIntervals = "At Regular Intervals"
+        case onSpecificDaysOfTheWeek = "On Specific Days of the Week"
+        case asNeeded = "As Needed"
+    }
+    
+    enum MedicineChosenInterval: String {
+        case day = "Day"
+        case otherDay = "Other Day"
+        case threeDays = "3 Days"
+        case fourDays = "4 Days"
+        case ninetyNineDays = "99 Days"
+    }
+    
+    enum MedicineChosenDay: String {
+        case Monday = "Mon"
+        case Tuesday = "Tue"
+        case Wednesday = "Wed"
+        case Thursday = "Thu"
+        case Friday = "Fri"
+        case Saturday = "Sat"
+        case Sunday = "Sun"
+    }
+    
 }
