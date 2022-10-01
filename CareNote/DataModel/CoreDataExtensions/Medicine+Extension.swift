49a813e4c242ee71ec73dcee62aa11d098abbbf4
Christian Schinkel <christian.schinkel@me.com>
2022-10-02 00:04:37 +0200

    Almost all medicine properties are added.

--- CareNote/DataModel/CoreDataExtensions/Medicine+Extension.swift
+++ CareNote/DataModel/CoreDataExtensions/Medicine+Extension.swift
@@ -10,15 +10,45 @@ import CoreData
 
 extension Medicine {
     /// Is a faster way to initialise only properties that are needed. So you don't need to initialise all properties.
-    convenience init(date: Date, name: String, form: String, frequency: String, strength: Double, unit: String, amount: Double, context: NSManagedObjectContext) {
-        self.init(context: context)
-        self.date_ = date
-        self.name_ = name
-        self.form_ = form
-        self.frequency_ = frequency
-        self.strength_ = strength
-        self.unit_ = unit
-        self.amount_ = amount
+    convenience init(
+        date: Date,
+        dateGiven: Date,
+        datePrescriptionIsEnding: Date,
+        dateSkipped: Date,
+        
+        name: String,
+        form: String,
+        frequency: String,
+        strength: Double,
+        unit: String,
+        amount: Double,
+        
+        instruction: String,
+        
+        isGiven: Bool,
+        isPrescripted: Bool,
+        isSkipped: Bool,
+        
+        context: NSManagedObjectContext) {
+            self.init(context: context)
+            
+            self.date_ = date
+            self.dateGiven_ = dateGiven
+            self.datePrescriptionIsEnding_ = datePrescriptionIsEnding
+            self.dateSkipped_ = dateSkipped
+            
+            self.name_ = name
+            self.form_ = form
+            self.frequency_ = frequency
+            self.strength_ = strength
+            self.unit_ = unit
+            self.amount_ = amount
+            
+            self.instruction_ = instruction
+            
+            self.isGiven_ = isGiven
+            self.isPrescripted_ = isPrescripted
+            self.isSkipped_ = isSkipped
     } // <- It's a priority fast lane.
     
     
@@ -29,8 +59,8 @@ extension Medicine {
      date
      name
      */
-    
-    
+// MARK: Dates here
+    /// Medicine's prescription-date.
     public var date: Date {
         get {
             date_ ?? Date.now
@@ -39,6 +69,34 @@ extension Medicine {
             date_ = newValue
         }
     }
+    /// Medicine's given-date
+    public var dateGiven: Date {
+        get {
+            dateGiven_ ?? Date.now
+        }
+        set {
+            dateGiven_ = newValue
+        }
+    }
+    /// Medicine's prescription's ending-date.
+    public var datePrescriptionIsEnding: Date {
+        get {
+            datePrescriptionIsEnding_ ?? Date.distantFuture
+        }
+        set {
+            datePrescriptionIsEnding_ = newValue
+        }
+    }
+    /// Medicine's dateSkipped_.
+    public var dateSkipped: Date {
+        get {
+            dateSkipped_ ?? Date.now
+        }
+        set {
+            dateSkipped_ = newValue
+        }
+    }
+// MARK: Medicine-information here
     /// The name of medicine.
     public var name: String {
         get {
@@ -66,7 +124,6 @@ extension Medicine {
             frequency_ = newValue
         }
     }
-    
     /// The strength for medicine.
     public var strength: Double {
         get {
@@ -76,7 +133,7 @@ extension Medicine {
             strength_ = newValue
         }
     }
-    /// The unit of medicine's strength.
+    /// The unit of medicine's unit.
     public var unit: String {
         get {
             unit_ ?? ""
@@ -95,26 +152,83 @@ extension Medicine {
         }
     }
     
+    /// Instructions for intake of medicine.
+    public var instruction: String {
+        get {
+            instruction_ ?? ""
+        }
+        set {
+            instruction_ = newValue
+        }
+    }
+// MARK: Bolean here
+    /// Is medicine given?
+    public var isGiven: Bool {
+        get {
+            isGiven_
+        }
+        set {
+            isGiven_ = newValue
+        }
+    }
+    /// Is medicine given?
+    public var isPrescripted: Bool {
+        get {
+            isPrescripted_
+        }
+        set {
+            isPrescripted_ = newValue
+        }
+    }
+    /// Is medicine given?
+    public var isSkipped: Bool {
+        get {
+            isSkipped_
+        }
+        set {
+            isSkipped_ = newValue
+        }
+    }
     /// forKey Medicine-property names to reduce typing misstakes, when using it for stringly typed names or properties.
     struct MedicineProperties {
         static let date = "date_"
+        static let dateGiven = "dateGiven_"
+        static let datePrescriptionIsEnding = "datePrescriptionIsEnding_"
+        static let dateSkipped = "dateSkipped_"
+        
         static let form = "form_"
         static let frequency = "frequency_"
         static let name = "name_"
         static let strength = "strength_"
         static let unit = "unit_"
         static let amount = "amount_"
+        
+        static let instruction = "instruction_"
+        
+        static let isGiven = "isGiven_"
+        static let isPrescripted = "isPrescripted_"
+        static let isSkipped = "isSkipped_"
     }
     
     /// Adds a value of value-type [Date(), String(), etc ] to the property with the stringly typed name.
     public override func awakeFromInsert() {
         setPrimitiveValue(Date.now, forKey: MedicineProperties.date)
+        setPrimitiveValue(Date.now, forKey: MedicineProperties.dateGiven)
+        setPrimitiveValue(Date.distantFuture, forKey: MedicineProperties.datePrescriptionIsEnding)
+        setPrimitiveValue(Date.now, forKey: MedicineProperties.dateSkipped)
+        
         setPrimitiveValue("", forKey: MedicineProperties.form)
         setPrimitiveValue("", forKey: MedicineProperties.frequency)
         setPrimitiveValue("", forKey: MedicineProperties.name)
         setPrimitiveValue(Double(), forKey: MedicineProperties.strength)
         setPrimitiveValue("", forKey: MedicineProperties.unit)
         setPrimitiveValue(Double(), forKey: MedicineProperties.amount)
+        
+        setPrimitiveValue("", forKey: MedicineProperties.instruction)
+        
+        setPrimitiveValue(Bool(), forKey: MedicineProperties.isGiven)
+        setPrimitiveValue(Bool(), forKey: MedicineProperties.isPrescripted)
+        setPrimitiveValue(Bool(), forKey: MedicineProperties.isSkipped)
     }
     /// Deletes law from the list att the current position.
     static func delete(at offsets: IndexSet, for medicine: [Medicine]) {
@@ -134,7 +248,7 @@ extension Medicine {
     }
     /// Used to create en example in the preview-canvas. Useful to create an example with an array or many relationships.
     static func example(context: NSManagedObjectContext) -> Medicine {
-        Medicine(date: Date.now, name: "ExampleMedicine", form: "Tablet", frequency: "1", strength: 1.0, unit: "g", amount: 1.0, context: context)
+        Medicine(date: Date.now, dateGiven: Date.now, datePrescriptionIsEnding: Date.distantFuture, dateSkipped: Date.now, name: "ExampleMedicine", form: "Tablet", frequency: "1", strength: 1.0, unit: "g", amount: 1.0, instruction: "ExampleInstruction", isGiven: false, isPrescripted: false, isSkipped: false, context: context)
     }
 
 }
--- CareNote/Views/MedicineViews/AddMedicineView.swift
+++ CareNote/Views/MedicineViews/AddMedicineView.swift
@@ -17,34 +17,51 @@ struct AddMedicineView: View {
 // MARK: - local state properties for the sheet
     
     
-    @State private var date = Date.now
-    @State private var name = ""
+    @State private var date = Date.now // Date of Prescription.
+    @State private var datePrescriptionIsEnding = Date.distantFuture // Date Prescription is ending.
+    @State private var dateGiven = Date.now // Date the medicine is given.
+    @State private var dateSkipped = Date.now // Date the medicine is skipped.
+    @State private var name = "" // Name of medicine or substans.
     @State private var form: Care.Medicine.Form = .tablet
     @State private var frequency = ""
     @State private var strength = 1.0
     @State private var unit: Care.Medicine.Unit = .milliGram
     @State private var amount = 1.0
-    @State private var instruction = ""
+    @State private var instruction = "" // additional instructions for administration.
     @State private var isPrescripted = false
-    @State private var isTaken = false
+    @State private var isGiven = false
     @State private var isSkipped = false
     
     var body: some View {
         NavigationView {
             VStack {
                 Form {
-                    HStack {
-                        DatePicker("Please enter a date", selection: $date)
-                            .labelsHidden()
-                        Spacer()
+                    VStack {
+                        Toggle(isOn: $isPrescripted) { // whether it is prescribed or not.
+                            Text("Prescripted On:")
+                        }
+                        
+                        HStack {
+                            DatePicker("Please enter a date", selection: $date) // Prescription-date.
+                                .labelsHidden()
+                            Spacer()
+                        }
+                        
+                        HStack {
+                            DatePicker("Ending on:", selection: $datePrescriptionIsEnding) // Prescription ending on date.
+                            Spacer()
+                        }
                     }
+                    
                     TextField("Medication name", text: $name)
                     Picker(selection: $form, label: Text("Medication form")) {
                         ForEach(Care.Medicine.Form.allCases, id: \.self) { form in
                             Text(form.name)
                         }
                     }
+                    
                     TextField("Medication frequency", text: $frequency)
+                    
                     HStack {
                         TextField("Strength", value: $strength, format: .number)
                             .keyboardType(.decimalPad)
@@ -68,8 +85,33 @@ struct AddMedicineView: View {
                         
                     }
                     
+                    VStack {
+                        Toggle(isOn: $isGiven) {
+                            Text("Given on:")
+                        }
+                        .onTapGesture(perform: changeToggle)
+                        
+                        HStack {
+                            DatePicker("Please enter a date", selection: $dateGiven) // Date medicine is given.
+                                .labelsHidden()
+                            Spacer()
+                        }
+                    }
                     
+                    VStack {
+                        Toggle(isOn: $isSkipped) {
+                            Text("Skipped On:")
+                        }
+                        .onTapGesture(perform: changeToggle)
+                        
+                        HStack {
+                            DatePicker("Please enter a date", selection: $dateSkipped) // Date medicine is skipped.
+                                .labelsHidden()
+                            Spacer()
+                        }
+                    }
                     
+                    TextField("Instructions", text: $instruction)
                 }
             }
             .navigationTitle("Add Medicine")
@@ -92,9 +134,37 @@ struct AddMedicineView: View {
         }
     }
 // MARK: - Functions for this View
+    /// Toggles the bolean of the toggles "isSkipped" and "isGiven"
+    private func changeToggle() {
+        if isSkipped == false {
+            if isGiven == true {
+                isGiven.toggle()
+            }
+        } else if isGiven == false {
+            isSkipped.toggle()
+        }
+    }
+    
     private func addMedicine() {
         withAnimation {
-            patient.addToMedicine(Medicine(date: date, name: name, form: form.name, frequency: frequency, strength: strength, unit: unit.rawValue, amount: 1.0, context: viewContext)) //_ = Medicine(date: date, name: name, context: viewContext)
+            patient.addToMedicine(
+                Medicine(
+                    date: date,
+                    dateGiven: dateGiven,
+                    datePrescriptionIsEnding: datePrescriptionIsEnding,
+                    dateSkipped: dateSkipped,
+                    name: name,
+                    form: form.name,
+                    frequency: frequency,
+                    strength: strength,
+                    unit: unit.rawValue,
+                    amount: amount,
+                    instruction: instruction,
+                    isGiven: isGiven,
+                    isPrescripted: isPrescripted,
+                    isSkipped: isSkipped,
+                    context: viewContext)) //_ = Medicine(date: date, name: name, context: viewContext)
+            
             PersistenceController.shared.save() // Saves the patient.
            // print("Patient: \(name) \(familyName) \(swedishSocialSecurityNumber) adds on the list.")
            dismiss()
