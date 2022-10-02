16546024633d611dc0b764351a5fc9d51349917a
Christian Schinkel <christian.schinkel@me.com>
2022-10-02 20:43:37 +0200

    added a picker on medicineList. But isn't fully implemented.

--- CareNote/Views/MedicineViews/MedicineListView.swift
+++ CareNote/Views/MedicineViews/MedicineListView.swift
@@ -14,31 +14,41 @@ struct MedicineListView: View {
     @State private var showingAddMedicineView = false // by default the AddMedicineView isn't presented.
     
     var body: some View {
+        VStack {
+            // TODO: Make this picker changing witch list-option should be shown - Prescripted medicine, given medicine and skipped medicine.
+            Picker(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/, label: /*@START_MENU_TOKEN@*/Text("Picker")/*@END_MENU_TOKEN@*/) {
+                Text("Prescripted").tag(1)
+                Text("Given").tag(2)
+                Text("Skipped").tag(3)
+            }
+            .pickerStyle(.segmented)
+            
             List {
-                ForEach(patient.medicineArray) { medicine in
-                    NavigationLink {
-                        MedicineDetailView(medicine: medicine) // Destination
-                    } label: {
-                        MedicineRowView(patient: patient, medicine: medicine) // Label in the List
+                    ForEach(patient.medicineArray) { medicine in
+                        NavigationLink {
+                            MedicineDetailView(medicine: medicine) // Destination
+                        } label: {
+                            MedicineRowView(patient: patient, medicine: medicine) // Label in the List
+                        }
+                    }
+                    .onDelete(perform: deleteMedicine)
+                }
+                .navigationBarTitle("Medicine")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        EditButton()
+                    }
+                    
+                    ToolbarItem {
+                        Button(action: showAddMedicineView) {
+                            Label("Add Medicine", systemImage: "plus")
+                        }
                     }
                 }
-                .onDelete(perform: deleteMedicine)
+                .sheet(isPresented: $showingAddMedicineView) {
+                    AddMedicineView(patient: patient)
             }
-            .navigationBarTitle("Medicine")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                
-                ToolbarItem {
-                    Button(action: showAddMedicineView) {
-                        Label("Add Medicine", systemImage: "plus")
-                    }
-                }
-            }
-            .sheet(isPresented: $showingAddMedicineView) {
-                AddMedicineView(patient: patient)
         }
     }
 // MARK: - Functions for this View
